40c2abd52986d0b808c6635621a6d7184f6e1e3d
VuceticBranislav <24853106+VuceticBranislav@users.noreply.github.com>
2022-11-26 16:14:39 +0100

    BaseHttpService fix

--- src/services/base-http.service.ts
+++ src/services/base-http.service.ts
@@ -7,31 +7,31 @@ const BaseHttpService = () => {
   let _accessToken: string | null = null;
 
   const get = async (endpoint:string, options = {}) => {
-    options = {...options, ..._getCommonOptions()};
+    options = _updateOptions(options);
     return await axios.get(`${BASE_URL}/${endpoint}`, options)
       .catch(error => _handleHttpError(error));
   }
 
   const post = async (endpoint:string, data = {}, options = {}) => {
-    options = {...options, ..._getCommonOptions()};
+    options = _updateOptions(options);
     return axios.post(`${BASE_URL}/${endpoint}`, data, options)
       .catch(error => _handleHttpError(error));
   }
 
   const put = async (endpoint:string, data = {}, options = {}) => {
-    options = {...options, ..._getCommonOptions()};
+    options = _updateOptions(options);
     return axios.put(`${BASE_URL}/${endpoint}`, data, options)
       .catch(error => _handleHttpError(error));
   }
 
   const remove = async (endpoint:string, options = {}) => {
-    options = {...options, ..._getCommonOptions()};
+    options = _updateOptions(options);
     return axios.delete(`${BASE_URL}/${endpoint}`, options)
       .catch(error => _handleHttpError(error));
   }
 
   const patch = async (endpoint:string, data = {}, options = {}) => {
-    options = {...options, ..._getCommonOptions()};
+    options = _updateOptions(options);
     return axios.patch(`${BASE_URL}/${endpoint}`, data, options)
       .catch(error => _handleHttpError(error));
   }
@@ -47,6 +47,13 @@ const BaseHttpService = () => {
         Authorization: `Bearer ${token}`,
       },
     };
+  }
+
+  const _updateOptions = (options = {}) => {
+    let optionsHeaders = (options as any).headers;
+    const commonOptions = _getCommonOptions();
+    optionsHeaders = {...optionsHeaders, ...commonOptions.headers};
+    return options = {...options, ...commonOptions, ...{headers: optionsHeaders}};
   }
 
   const getAccessToken = () => {
